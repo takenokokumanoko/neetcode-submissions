c261a1ac95624f8965b52c89b778dbc1c6ee2e65
たけくま <yuka.17uni@gmail.com>
2026-09-20 22:04:56 +0900

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.ts
@@ -0,0 +1,55 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        const anagramSMap = new Map<string, number>;
+        const anagramTMap = new Map<string, number>;
+        if(s.length != t.length) {
+            return false;
+        }
+        for(let i=0; i<s.length; i++) {
+            anagramSMap.set(s[i], (anagramSMap.get(s[i]) ?? 0) + 1);
+            anagramTMap.set(t[i], (anagramTMap.get(t[i]) ?? 0) + 1);
+        }
+
+        for(const [key, value] of anagramSMap) {
+            if(value !== (anagramTMap.get(key) ?? 0)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //     for(let i=0; i<s.length; i++) {
+    //         const word = s.slice(i, i+1);
+    //         if(anagramSMap.has(word)) {
+    //             anagramSMap.set(word, anagramSMap.get(word) + 1);
+    //         }else{
+    //             anagramSMap.set(word, 1);
+    //         }
+    //     }
+    //     for(let j=0; j<t.length; j++) {
+    //         const word = t.slice(j, j+1);
+    //         if(anagramTMap.has(word)) {
+    //             anagramTMap.set(word, anagramTMap.get(word) + 1);
+    //         }else{
+    //             anagramTMap.set(word, 1);
+    //         }
+    //     }
+    //     if(anagramSMap.size !== anagramTMap.size) {
+    //         return false;
+    //     }
+    //     for(const [key, value] of anagramSMap.entries()) {
+    //         if(anagramTMap.has(key)) {
+    //             if(value === anagramTMap.get(key)) {
+    //                 continue;
+    //             }
+    //         }
+    //         return false;
+    //     }
+    //     return true;
+    // }
+}

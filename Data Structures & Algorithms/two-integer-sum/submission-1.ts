bfad98b581f0e82b006d4c6f21befb4b17f547c2
たけくま <yuka.17uni@gmail.com>
2026-09-21 18:05:51 +0900

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        //nums.sort((a, b) => a-b);
+        //console.log(nums);
+        for(let i=0; i<nums.length-1; i++) {
+            for(let j=i+1; j<nums.length; j++) {
+                if(nums[i] + nums[j] === target) {
+                    return [i, j];
+                }
+            }
+        }
+        return [];
+    }
+}
